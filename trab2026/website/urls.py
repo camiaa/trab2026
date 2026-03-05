28e1c321eb63a34c7328edbde080b17aeaa573cc
unknown <camilascriptore33@gmail.com>
2026-03-05 09:19:23 -0300

    Mensagem do que foi feito

--- trab2026/website/urls.py
+++ trab2026/website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from.views import Index
+from.views import Index, Sobre, Contato
 
 urlpatterns = [
    # path("admin/", admin.site.urls),
-   path("inicio", Index.as_view(), name="pagina_inicial")
+   path("inicio/", Index.as_view(), name="pagina_inicial"),
+   path("sobre/", Sobre.as_view(), name="sobre"),
+    path("contato/", Contato.as_view(), name="contato"),
 ]
